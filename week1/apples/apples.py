f13bc83075bbc4e5b48cd8e5c5d1ecdf181f2753
jess smith <jessica.k.smith@bryanuniversity.edu>
2022-07-12 14:58:31 -0600

    week1

---
+++ week1/apples/apples.py
@@ -0,0 +1,14 @@
+
+applePrice = 0.50
+
+name = input("What is your name? ")
+
+print(name)
+
+message = input('Hello '+name+' Apples cost $0.50 each. How many would you like?')
+
+#print(message)
+
+thankmsg = "Thank you for your purchase of " +message+ " apples at $0.50 each"
+
+print(thankmsg)
