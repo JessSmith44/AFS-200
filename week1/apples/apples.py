507669cf789d8f28cc4154701b16240b1c77f80c
jess smith <jessica.k.smith@bryanuniversity.edu>
2022-07-13 14:14:08 -0600

    corrected

--- week1/apples/apples.py
+++ week1/apples/apples.py
@@ -5,10 +5,10 @@ name = input("What is your name? ")
 
 print(name)
 
-message = input('Hello '+name+' Apples cost $0.50 each. How many would you like?')
+message = int(input(f'Hello {name} Apples cost ${applePrice:,.2f} each. How many would you like?'))
 
 #print(message)
 
-thankmsg = "Thank you for your purchase of " +message+ " apples at $0.50 each"
+thankmsg = f"Thank you for your purchase of {message} apples at ${applePrice:,.2f} each. Your total today is ${(message * applePrice):,.2f}"
 
 print(thankmsg)
