1ce18194acba0bf07a01bbf8b7322c675e2e7c4d
jess smith <jessica.k.smith@bryanuniversity.edu>
2022-08-24 18:09:40 -0600

    week 7

--- week6/addressbook/contacts/views.py
+++ week6/addressbook/contacts/views.py
@@ -15,13 +15,21 @@ def show_all_contacts(request):
 
 def show_contact(request, contactid):
     try:
-        # contacts = Contact.objects.filter()
-        # template = loader.get_template('index.html')
-        # context = {
-        #     'contacts' : contacts
-        # }
-        # return HttpResponse(template.render(context, request))
-        contactid = Contact.objects.get(id=contactid)
-        return HttpResponse(f'<h1>Showing details for "{contactid}"</h1>')
+        # contactid = Contact.objects.get(id=contactid)
+        # return HttpResponse(f'<h1>Showing details for "{contactid}"</h1>')
+        template = loader.get_template('index.html')
+        contacts = Contact.objects.all()
+        def func(contact):
+            print(contact.id)
+            if(contact.id == contactid):
+                return True
+            else:
+                return False
+
+        oneContact = filter(func, contacts)
+        context = {
+            'contacts' = oneContact
+        }
+        return HttpResponse(template.render(context, request))
     except Contact.DoesNotExist:
         return HttpResponse(f'<h1>Unable to locate contact {contactid}</h1>')
